c07def77543dc0d1dd7cb4c89dbace2c017b7715
PrincessPiggy22 <mlpuryear@my.waketech.edu>
2026-04-15 10:13:40 -0400

    Create Main class with Pokemon team setup
    Initial implementation of the Main class with a team of Pokemon.

---
+++ Code/Main.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    // What I need
+    // Pokemon class
+    // Move class/method
+    // Round system
+    // Trainer
+    // List of player's Pokemon
+    // A way to get more pokemon
+    // Different rooms
+    // display states
+
+    static Mon[] team = new Mon[6];
+    Mon Zumbah = new Mon("Zumbah","Purple",50);
+    Mon ZZZ = new Mon("Zing Zing Zingbah", "God",1500);
+    Mon Jumbah = new Mon("Jumbah", "Water", 50);
+    Mon Humbah = new Mon("Humbah", "Electric",80);
+    Mon Jingbah = new Mon("Jingbah", "Fairy", 80);
+    Mon Phil = new Mon("Phil Collins", "God", 1000);
+    Mon Yoda = new Mon("Jedi Master Yoda", "God", 100);
+
+
+    public void battle(){
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
