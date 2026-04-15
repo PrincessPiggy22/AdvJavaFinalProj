db2d584f83ccec2446929b4cd1258bb83b84b10c
PrincessPiggy22 <mlpuryear@my.waketech.edu>
2026-04-15 10:14:47 -0400

    Create Move class with attributes and constructor

---
+++ Code/Move.java
@@ -0,0 +1,15 @@
+public class Move {
+
+    String name;
+    String type;
+    int dmg;
+    int uses;
+
+    Move(String name, String type, int dmg, int uses){
+        this.name = name;
+        this.type = type;
+        this.dmg = dmg;
+        this.uses = uses;
+    }
+
+}
