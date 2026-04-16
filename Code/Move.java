735bc8f85746ab425c5032cf0d6e586c8854095e
PrincessPiggy22 <mlpuryear@my.waketech.edu>
2026-04-16 10:07:11 -0400

    Refactor Move class constructor to remove type
    Removed 'type' parameter from Move constructor.

--- Code/Move.java
+++ Code/Move.java
@@ -1,13 +1,11 @@
 public class Move {
 
     String name;
-    String type;
     int dmg;
     int uses;
 
-    Move(String name, String type, int dmg, int uses){
+    Move(String name, int dmg, int uses){
         this.name = name;
-        this.type = type;
         this.dmg = dmg;
         this.uses = uses;
     }
