4f010fdf7c3c5eb07a98dd183df0a6c4e6ecde98
PrincessPiggy22 <mlpuryear@my.waketech.edu>
2026-04-16 10:08:01 -0400

    Refactor Main.java to add chooseMove and round methods

--- Code/Main.java
+++ Code/Main.java
@@ -14,22 +14,19 @@ public class Main {
     // display states
 
     static Mon[] team = new Mon[6];
-    Mon Zumbah = new Mon("Zumbah","Purple",50);
-    Mon ZZZ = new Mon("Zing Zing Zingbah", "God",1500);
-    Mon Jumbah = new Mon("Jumbah", "Water", 50);
-    Mon Humbah = new Mon("Humbah", "Electric",80);
-    Mon Jingbah = new Mon("Jingbah", "Fairy", 80);
-    Mon Phil = new Mon("Phil Collins", "God", 1000);
-    Mon Yoda = new Mon("Jedi Master Yoda", "God", 100);
 
+    public void chooseMove(){
 
-    public void battle(){
-        System.out.println();
+    }
+
+    public void round(){
+
     }
 
     public static void main(String[] args) {
 
-
+        Mon.printMove(Mon.moveOne);
+        Mon.printStats();
 
     }
 }
